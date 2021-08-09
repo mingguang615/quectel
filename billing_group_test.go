0a668d17830ff89249d724522b15429ada72a0ca
mingguang615 <w@163.com>
2021-08-09 15:36:57 +0800

    panic err

--- billing_group_test.go
+++ billing_group_test.go
@@ -5,8 +5,7 @@ import "testing"
 func TestClient_GetBillingGroupList(t *testing.T) {
 	resp, err := c.GetBillingGroupList()
 	if err != nil {
-		t.Log(err)
-		return
+		panic(err)
 	}
 	t.Log(resp)
 }
